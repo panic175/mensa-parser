ad0fca82b84fdfca1952ec2f90c46da290755a98
Patrick Niemann <niemann.patrick@gmail.com>
2015-12-10 14:28:25 +0100

    Projektdateien hinzugefügt

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+define('WEBSITE', 'http://www.studentenwerk-oldenburg.de/de');
+require_once ('simple_html_dom.php');
+require_once ('functions.php');
+
+$uri = (isset($_GET['uri']) ? $_GET['uri'] : NULL); // ?uri=/gastronomie/speiseplaene/uhlhornsweg-ausgabe-b.html
+
+if (empty($uri)) {
+    $mensen = getMensaUris('/gastronomie/speiseplaene.html');
+} else {
+    $mensen = array($uri);
+    unset($uri);
+}
+$output = array();
+foreach ($mensen as $mensaName => $mensaUri) {
+	//var_dump(getLunchObj($mensa));
+	$output[sanitize_title_with_dashes($mensaName)] = json_decode(getJson($mensaUri, '120 minutes'));
+}
+echo json_encode($output);
